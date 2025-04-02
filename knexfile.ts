4ab1a0c3c4f08c0e49c852ac80365b278ce17c49
Augusto-02 <augustonodari@gmail.com>
2025-04-01 23:53:11 -0300

    Update knexfile.ts

--- knexfile.ts
+++ knexfile.ts
@@ -2,7 +2,7 @@ import type { Knex } from "knex";
 import { env } from "./src/env";
 
 const config: Knex.Config = {
-  client: env.DATABE_CLIENT,
+  client: env.DATABASE_CLIENT,
   connection:
     env.DATABASE_URL == "sqLite"
       ? { filename: env.DATABASE_URL }
