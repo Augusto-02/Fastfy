2fbf67cca8c0324c9e7eabae6951531594e9c591
Augusto-02 <augustonodari@gmail.com>
2025-04-02 00:29:10 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
 import { env } from "./env";
 
+const port = env.PORT || 3000;
+const host = "RENDER" in process.env ? `0.0.0.0` : `localhost`;
+
 app
-  .listen({
-    port: env.PORT,
-  })
+  .listen({ host: host, port: port })
   .then(() => console.log("Server is Running"));
